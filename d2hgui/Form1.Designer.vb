bbf7dfef6ebae3993d89adf48386a7602caeea3a
L. C. Norberg <agent.blockhead@inbox.com>
2016-04-14 18:22:56 -0700

    The First Commit

--- d2hgui/Form1.Designer.vb
+++ d2hgui/Form1.Designer.vb
@@ -22,9 +22,135 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.DefaultExt = "mp3"
+        Me.OpenFileDialog1.FileName = "music.mp3"
+        Me.OpenFileDialog1.Filter = "All files|*.*"
+        Me.OpenFileDialog1.Title = "Find your music"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(85, 66)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(85, 42)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Start Sample"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "End Sample"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(85, 16)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(379, 20)
+        Me.TextBox3.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 19)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Filename"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(85, 92)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "♪"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(202, 45)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(287, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Custom looping will be enabled in a later release of dsp2hps"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(470, 14)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Browse..."
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(557, 127)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "d2hgui"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button2 As Button
 End Class
--- d2hgui/Form1.vb
+++ d2hgui/Form1.vb
@@ -1,3 +1,57 @@
 ﻿Public Class Form1
+    Public Shared progdir As String = My.Computer.FileSystem.CurrentDirectory
+    Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        OpenFileDialog1.ShowDialog()
+    End Sub
 
+    Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        Dim strm As System.IO.Stream
+        Dim Originalfile As String
+        strm = OpenFileDialog1.OpenFile()
+        TextBox3.Text = OpenFileDialog1.FileName.ToString()
+        Originalfile = OpenFileDialog1.FileName.ToString()
+        If Not (strm Is Nothing) Then
+            ' would do something here but it's not necessary.
+        End If
+    End Sub
+
+    Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Originalfile As String
+        Originalfile = OpenFileDialog1.FileName.ToString()
+        Dim hpsvars As String = " -y -i """ + Originalfile + """ -ar 32000 -acodec pcm_s16le -map_channel 0.0.0 LEFT.wav -ar 32000 -acodec pcm_s16le -map_channel 0.0.1 RIGHT.wav"
+        Using runffmpeg As New Process
+            runffmpeg.StartInfo.Arguments = hpsvars
+            runffmpeg.StartInfo.FileName = progdir + "\tool\ffmpeg.exe"
+            runffmpeg.Start()
+            runffmpeg.WaitForExit()
+        End Using
+        Using dspleft As New Process
+            dspleft.StartInfo.Arguments = "-e left.wav left.dsp"
+            dspleft.StartInfo.FileName = progdir + "\tool\dspadpcm.exe"
+            dspleft.Start()
+            dspleft.WaitForExit()
+        End Using
+        Using dspright As New Process
+            dspright.StartInfo.Arguments = "-e right.wav right.dsp"
+            dspright.StartInfo.FileName = progdir + "\tool\dspadpcm.exe"
+            dspright.Start()
+            dspright.WaitForExit()
+        End Using
+        Using d2hmake As New Process
+            d2hmake.StartInfo.Arguments = "--left_dsp LEFT.dsp --right_dsp RIGHT.dsp -o """ + Originalfile + ".hps"""
+            d2hmake.StartInfo.FileName = progdir + "\tool\dsp2hps.exe"
+            d2hmake.Start()
+            d2hmake.WaitForExit()
+        End Using
+        IO.File.Delete(progdir + "\tool\left.wav")
+        IO.File.Delete(progdir + "\tool\right.wav")
+        IO.File.Delete(progdir + "\tool\left.dsp")
+        IO.File.Delete(progdir + "\tool\right.dsp")
+        IO.File.Delete(progdir + "\tool\left.txt")
+        IO.File.Delete(progdir + "\tool\right.txt")
+    End Sub
+
+    Public Sub WaitForExit()
+
+    End Sub
 End Class
